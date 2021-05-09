6d25474d09af761af54aa64bdecff299e81dfe8d
mranido <mranido.da@gmail.com>
2021-05-09 20:27:32 +0200

    Add envio correo

--- backend/api/components/contacts/controller/crete-contact.js
+++ backend/api/components/contacts/controller/crete-contact.js
@@ -1,34 +1,40 @@
-'use strict';
+"use strict";
 const Joi = require("joi");
 const schema = require("../schemas");
 const model = require("../../../infrastructure/mock-db");
-const response = require("../../../routes/response");
-const TABLE ='contacts';
-const TABLE2 = 'users';
+const responses = require("../../../routes/responses");
+const { sendEmailContact } = require("../../../helpers/mail-smtp");
+const TABLE = "contacts";
+const TABLE2 = "users";
 
 async function createContact(req, res, next) {
   try {
+    const { id } = req.params;
 
-    const {id} = req.params;
-    const userId = Number(id);
     await schema.create.validateAsync(req.body);
     const { contactScoutId, contactPlayerId, contactDescription } = req.body;
 
-
-    const player = await model.findOne({userId}, TABLE2);
+    const player = await model.findOne({ id }, TABLE2);
     if (!player) {
-      return response.error(req, res,"Persona no existe", 400);
+      return responses.error(req, res, "Persona no existe", 400);
     }
+    const { userEmail, userName } = player;
 
-    const contactDB ={ contactScoutId ,contactPlayerId: userId ,contactDescription }
+    const contactDB = {
+      contactScoutId,
+      contactPlayerId: id,
+      contactDescription,
+    };
 
     const createAComment = await model.create(contactDB, TABLE);
 
-    response.success(req, res, "mensaje creado", 201);
+    await sendEmailContact(userName, userEmail);
 
+    responses.success(req, res, "mensaje creado", 201);
   } catch (error) {
     next(error);
   }
 }
+
 
 module.exports = createContact;
